3e345da960d94f8484486c78883d4a4d9be64397
Takumi Kohge <26m207@st.takushoku-u.ac.jp>
2026-07-02 10:23:28 +0900

    2026-07-02

--- bitstream.hpp
+++ bitstream.hpp
@@ -9,12 +9,12 @@ class bitstream {
   std::vector<uint8_t> stream;
 
  public:
-  // コンストラクタ(クラスの名前を使う, クラス実体化の際に最初に呼ばれる)
-  bitstream() : tmp(0), bits(0), stream({}) { stream.reserve(32768); }
-  // デストラクタ(クラスの実態の寿命が切れた際に呼ばれる)
+  // コンストラクタ: クラスの実体化の際に最初に呼ばれる
+  bitstream() : tmp(0), bits(0) { stream.reserve(32768); }
+  // デストラクタ: クラスの実態の寿命が切れた際に呼ばれる
   ~bitstream() {}
 
-  void put_bits(uint32_t cwd, uint32_t len) {
+  inline void put_bits(uint32_t cwd, uint32_t len) {
     uint8_t b;
     while (len > 0) {
       b = static_cast<uint8_t>((cwd >> (len - 1)) & 1);
@@ -23,30 +23,38 @@ class bitstream {
       len--;
       if (bits == 8) {
         put_byte(tmp);
+        if (tmp == 0xFF) {
+          // バイトスタッフィング処理
+          put_byte(0x00);
+        }
         tmp = 0;
         bits = 0;
       }
     }
   }
   void flush() {
-    tmp = tmp << (8 - bits);     // 　上位へ寄せる
-    tmp = tmp | (0xFF >> bits);  // 　下位bitを1で埋める
-
+    tmp = tmp << (8 - bits);     // 上位へ寄せる
+    tmp = tmp | (0xFF >> bits);  // 下位bitを1で埋める
     put_byte(tmp);
-  }
-
-  void put_byte(uint8_t val) {
-    stream.push_back(val);
-    if (val == 0xFF) {
+    if (tmp == 0xFF) {
       // バイトスタッフィング処理
-      stream.push_back(0x00);
+      put_byte(0x00);
     }
   }
 
-  void put_word(uint16_t val) {}
+  inline void put_byte(uint8_t val) { stream.push_back(val); }
+
+  inline void put_word(uint16_t val) {
+    // Big endian
+    put_byte(val >> 8);
+    put_byte(val & 0xFF);
+  }
 
   size_t finalize() {
     flush();
+    put_word(0xFFD9);  // EOI marker
     return stream.size();
   }
+
+  uint8_t* get_data() { return stream.data(); }
 };
--- cvtest.cpp
+++ cvtest.cpp
@@ -1,106 +1,24 @@
+#include <chrono>
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
 #include "bitstream.hpp"
-#include "huffman_tables.hpp"
+#include "coding.hpp"
+#include "jpgheaders.hpp"
 #include "qtable.hpp"
-#include "zigzag.hpp"
+#include "ycctype.hpp"
 
-constexpr int FWD = 0;
-constexpr int INV = 1;
 constexpr int Luma = 0;
 constexpr int Chroma = 1;
-constexpr int DC = 0;
-constexpr int AC = 1;
-
-template <class T>
-auto clamp = [](T val) {
-  if (val > 255) {
-    val = 255;
-  }
-  if (val < 0) {
-    val = 0;
-  }
-  return val;
-};
-
-cv::Mat rgb2ycbcr(cv::Mat img) {
-  // RGB -> YCbCr, imgはshallowコピーなのでOK
-  const int width = img.cols;
-  const int height = img.rows;
-  const int nc = img.channels();
-  const int stride = img.step;
-  uint8_t* pixel = img.data;
-  for (int i = 0; i < height; ++i) {
-    for (int j = 0; j < width; ++j) {
-      int B = pixel[i * stride + j * nc + 0];
-      int G = pixel[i * stride + j * nc + 1];
-      int R = pixel[i * stride + j * nc + 2];
-
-      int Y, Cb, Cr;
-      Y = .299 * R + .587 * G + .114 * B;
-      Cb = -.1687 * R - .3313 * G + .5 * B;
-      Cr = .5 * R - .4187 * G - .0813 * B;
-      Cb += 128;
-      Cr += 128;
-      pixel[i * stride + j * nc + 0] = clamp<int>(Y);
-      pixel[i * stride + j * nc + 1] = clamp<int>(Cr);
-      pixel[i * stride + j * nc + 2] = clamp<int>(Cb);
-    }
-  }
-  return img;
-}
-
-template <int X>
-void quantize(cv::Mat& blk, const float* qtable, float scale) {
-  const int width = blk.cols;
-  const int height = blk.rows;
-  const int nc = blk.channels();
-  const int stride = blk.step / sizeof(float);
-  float* pixel = reinterpret_cast<float*>(blk.data);
-  for (int i = 0; i < height; ++i) {
-    for (int j = 0; j < width; ++j) {
-      auto val = pixel[i * stride + j];
-      auto stepsize = clamp<float>(qtable[i * stride + j] * scale);
-      // 以下のif文はコンパイル時には消える
-      if (X == 0)  // 量子化
-        pixel[i * stride + j] = roundf(val / stepsize);
-      else  // 逆量子化
-        pixel[i * stride + j] = val * stepsize;
-    }
-  }
-}
-
-template <int X>
-// t_ はテーブル
-void encode(int run, int val, const unsigned int* t_cwd,
-            const unsigned int* t_len, bitstream& encbuf) {
-  int s = 0;
-  int uval = (val < 0) ? -val : val;  // uval は unsigned val
-  int bound = 1;                      // boundはカウンタ
-  while (uval >= bound) {
-    bound += bound;
-    s++;
-  }
-  if (X == 0) {
-    // DC
-    // t_cwd[s] と t_len[s] ビットの値としてビットストリームに書き込む
-    encbuf.put_bits(t_cwd[s], t_len[s]);
-  } else {
-    // AC
-    // t_cwd[(run << 4) + s] と t_len[(run << 4) + s]
-    // ビットの値としてビットストリームに書き込む -> 4bitのシステムを書き込む
-    encbuf.put_bits(t_cwd[(run << 4) + s], t_len[(run << 4) + s]);
-  }
-  if (s != 0) {
-    val = (val < 0) ? val - 1 : val;
-  }
-  //  val を sビットの値としてビットストリームに書き込む
-}
 
 int main(int argc, char* argv[]) {
-  cv::Mat image = cv::imread("./barbara.ppm", cv::IMREAD_ANYCOLOR);
+  if (argc < 3) {
+    printf("usage: cvtest.exe input output <quality> <444, 420, ...>\n");
+    exit(EXIT_FAILURE);
+  }
+  cv::Mat image = cv::imread(argv[1], cv::IMREAD_ANYCOLOR);
   if (image.empty()) return EXIT_FAILURE;
+  cv::Mat original = image.clone();  // PSNR算出のためにオリジナルを退避
   bitstream encbuf;
 
   const int nc = image.channels();
@@ -110,19 +28,42 @@ int main(int argc, char* argv[]) {
   std::vector<cv::Mat> ycrcb;
   cv::split(image, ycrcb);  // [0] -> Y, [1] -> Cr, [2] -> Cb
 
-  int dH = 2, dV = 2;  // 4:2:0
+  int YCCtype = YUV420;
+  if (argc > 4) {
+    if (!strcmp("444", argv[4])) {
+      YCCtype = YUV444;
+    } else if (!strcmp("422", argv[4])) {
+      YCCtype = YUV422;
+    } else if (!strcmp("411", argv[4])) {
+      YCCtype = YUV411;
+    } else if (!strcmp("440", argv[4])) {
+      YCCtype = YUV440;
+    } else if (!strcmp("420", argv[4])) {
+      YCCtype = YUV420;
+    } else if (!strcmp("410", argv[4])) {
+      YCCtype = YUV410;
+    } else if (!strcmp("GRAY", argv[4])) {
+      YCCtype = GRAY;
+    } else {
+      printf("Unknown YCC type\n");
+      exit(EXIT_FAILURE);
+    }
+  }
+  int dH = YCC_HV[YCCtype][0] >> 4, dV = YCC_HV[YCCtype][0] & 0x0F;
   cv::resize(ycrcb[1], ycrcb[1], cv::Size(), 1.0f / dH,
              1.0f / dV);  // 444 -> 420
   cv::resize(ycrcb[2], ycrcb[2], cv::Size(), 1.0f / dH,
              1.0f / dV);  // 444 -> 420
 
+  const int width = ycrcb[0].cols;
+  const int height = ycrcb[0].rows;
   // quality -> Qfactor -> scale
   int QF;
   int quality;
-  if (argc < 2) {
+  if (argc < 4) {
     quality = 75;
   } else {
-    quality = std::stoi(argv[1]);
+    quality = std::stoi(argv[3]);
   }
   quality = (quality == 0) ? 1 : quality;
   quality = (quality > 100) ? 100 : quality;
@@ -134,86 +75,80 @@ int main(int argc, char* argv[]) {
   QF = (QF == 0) ? 1 : QF;
   float scale = QF / 100.0f;
 
-  auto blkproc = [](cv::Mat tmp, const float* qmatrix, float scale,
-                    int& prev_dc, int c, bitstream& encbuf) {
-    cv::Mat blk;
-    tmp.convertTo(blk, CV_32F);
-    blk -= 128.0f;
-    // Forward DCT
-    cv::dct(blk, blk, FWD);
-    // 量子化
-    quantize<FWD>(blk, qmatrix, scale);
-
-    auto p = reinterpret_cast<float*>(blk.data);
-    // 現在のブロックDC成分から前のブロックのDC成分を引く
-    int diff = p[0] - prev_dc;
-    prev_dc = p[0];  // prev_dcを更新
-    encode<DC>(0, diff, DC_cwd[c], DC_len[c], encbuf);
-
-    // AC成分のためのジグザグスキャン・ハフマン符号化
-    int zero_run = 0;  // 0の連続数
-    for (int i = 1; i < 64; ++i) {
-      int ac = static_cast<int>(p[zigzag_scan[i]]);
-      if (ac == 0) {
-        zero_run++;
-      } else {
-        while (zero_run > 15) {
-          // ZRL
-          encode<AC>(0xf, 0x0, AC_cwd[c], AC_len[c], encbuf);
-          zero_run -= 16;
-        }
-        encode<AC>(zero_run, ac, AC_cwd[c], AC_len[c], encbuf);
-        zero_run = 0;
+  int qtable[2][64];
+  for (int i = 0; i < 64; ++i) {
+    if (QF != 1) {
+      qtable[0][i] = static_cast<int>(clamp<float>(qmatrix[0][i] * scale));
+      qtable[1][i] = static_cast<int>(clamp<float>(qmatrix[1][i] * scale));
+      if (qtable[0][i] == 0) {
+        qtable[0][i] = 1;
       }
+      if (qtable[1][i] == 0) {
+        qtable[1][i] = 1;
+      }
+    } else {
+      qtable[0][i] = qtable[1][i] = 1;
     }
-    if (zero_run) {
-      // EOB
-      encode<AC>(0x0, 0x0, AC_cwd[c], AC_len[c], encbuf);
-    }
+  }
 
-    // 逆量子化
-    quantize<INV>(blk, qmatrix, scale);
-    // Inverse DCT
-    cv::dct(blk, blk, INV);
-    blk += 128.0f;
-    blk.convertTo(tmp, CV_8U);
-    // エントロピー符号化
-  };
+  auto tic = std::chrono::high_resolution_clock::now();  // 時間計測開始
+  create_mainheader(width, height, nc, qtable[0], qtable[1], YCCtype, encbuf);
+  auto toc = std::chrono::high_resolution_clock::now();  // 時間計測終了
+  size_t duration =
+      std::chrono::duration_cast<std::chrono::microseconds>(toc - tic)
+          .count();  // 経過時間の算出
 
   // MCU(Minimum Coded Unit)単位の処理
-  const int width = ycrcb[0].cols;
-  const int height = ycrcb[0].rows;
-
-  // prev_dc[]にはひとつ前のブロックのDC成分値が入る
+  // prev_dc[]には前のブロックのDC成分値が入る
   int prev_dc[3] = {0};
-
   for (int y = 0, cy = 0; y < height; y += 8 * dV, cy += 8) {
     for (int x = 0, cx = 0; x < width; x += 8 * dH, cx += 8) {
       for (int i = 0; i < dV; ++i) {
         for (int j = 0; j < dH; ++j) {
           // 起点の座標(x, y)で、8x8の領域を切り出す
           cv::Mat tmpY = ycrcb[0](cv::Rect(x + j * 8, y + i * 8, 8, 8));
-          blkproc(tmpY, qmatrix[Luma], scale, prev_dc[0], Luma, encbuf);
+          blkproc(tmpY, qtable[Luma], prev_dc[0], Luma, encbuf);
         }
-        // 起点の座標(x, y)で、8x8の領域を切り出す
-        cv::Mat tmpCr = ycrcb[1](cv::Rect(cx, cy, 8, 8));  // Cr
-        blkproc(tmpCr, qmatrix[Chroma], scale, prev_dc[1], Chroma, encbuf);
-        cv::Mat tmpCb = ycrcb[2](cv::Rect(cx, cy, 8, 8));  // Cb
-        blkproc(tmpCb, qmatrix[Chroma], scale, prev_dc[2], Chroma, encbuf);
       }
+      // 起点の座標(cx, cy)で、8x8の領域を切り出す
+      cv::Mat tmpCr = ycrcb[2](cv::Rect(cx, cy, 8, 8));  // Cb
+      blkproc(tmpCr, qtable[Chroma], prev_dc[1], Chroma, encbuf);
+      cv::Mat tmpCb = ycrcb[1](cv::Rect(cx, cy, 8, 8));  // Cr
+      blkproc(tmpCb, qtable[Chroma], prev_dc[2], Chroma, encbuf);
     }
   }
-
   size_t length = encbuf.finalize();
-  std::cout << "codestream zize = " << length << std::endl;
+  // std::cout << "codestream size = " << length << ", ";
+  std::cout << static_cast<double>(length) * 8.0 / (width * height) << " ";
+  FILE* fp = fopen(argv[2], "wb");
+  if (fp == nullptr) {
+    printf("Cannot open file %s\n", argv[2]);
+    exit(EXIT_FAILURE);
+  }
+  fwrite(encbuf.get_data(), sizeof(uint8_t), length, fp);
+  fclose(fp);
 
   cv::resize(ycrcb[1], ycrcb[1], cv::Size(), dH, dV);
   cv::resize(ycrcb[2], ycrcb[2], cv::Size(), dH, dV);
   cv::merge(ycrcb, image);
   cv::cvtColor(image, image, cv::COLOR_YCrCb2BGR);
-  cv::imshow("loaded image", image);
 
-  cv::waitKey(0);
-  cv::destroyAllWindows();
+  double mse = 0.0;
+  for (int i = 0; i < height; ++i) {
+    for (int j = 0; j < width; ++j) {
+      for (int c = 0; c < nc; ++c) {
+        int v0 = original.data[i * original.step + j * nc + c];
+        int v1 = image.data[i * image.step + j * nc + c];
+        mse += (v0 - v1) * (v0 - v1);
+      }
+    }
+  }
+  mse /= (width * height * nc);
+  double psnr = 10 * log10(255 * 255 / mse);
+  std::cout << psnr << std::endl;
+  // cv::imshow("loaded image", image);
+
+  // cv::waitKey(0);
+  // cv::destroyAllWindows();
   return EXIT_SUCCESS;
 }
